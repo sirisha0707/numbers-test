b66aaaaf3cdb7eb1ed4928dddf3ff79e4c8be96c
sirisha0707 <sirisha.grandhe@gmail.com>
2020-01-30 14:55:42 +0530

    Create NumberGenerator.php

---
+++ NumberGenerator.php
@@ -0,0 +1,30 @@
+<?php
+class NumberGenerator {
+	/**
+	* Prints "Linio" for multiplies of 3, "IT" for multiplies of 5 and "Linianos" for multiplies of both 3 & 5
+	*
+	* @param int $start
+	* @param int $end
+	*
+	* @return void
+	*/
+	public static function numberPrint(int $start, int $end): void {
+		for ($num = $start; $num <= $end; $num++) {
+			switch($num) {
+				case ($num % 3 == 0 && $num % 5 == 0):
+					echo " Linianos ";
+					break;
+				case ($num % 3 == 0):
+					echo " Linio ";
+					break;
+				case ($num % 5 == 0):
+					echo " IT ";
+					break;
+				default:
+					echo " $num ";
+			}
+		}
+	}
+}
+
+NumberGenerator::numberPrint(1, 100);	// Calling the method
