7bfea822efa992d21a8de746476664d5583e56b7
sirisha0707 <sirisha.grandhe@gmail.com>
2020-01-30 14:55:08 +0530

    Create NumberGeneratorTest.php

---
+++ NumberGeneratorTest.php
@@ -0,0 +1,14 @@
+?php
+
+use PHPUnit\Framework\TestCase;
+
+class NumberGeneratorTest extends TestCase
+{
+    public function testNumberPrintSuccess(): void
+    {
+        $this->assertEquals(
+            'Linio 4 IT Linio 7 8 Linio IT ',
+            NumberGenerator::numberPrint(3, 10)
+        );
+    }
+}
