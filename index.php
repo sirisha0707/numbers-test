1ab748abcba4b64ee254afdb10e0bb7cb498506f
sirisha0707 <sirisha.grandhe@gmail.com>
2020-01-30 14:37:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,32 @@
 <?php
-/**
-* Prints "Linio" for multiplies of 3, "IT" for multiplies of 5 and "Linianos" for multiplies of both 3 & 5
-*
-* @param int $start
-* @param int $end
-*
-* @return void
-*/
-function numberPrint(int $start, int $end): void {
-    for ($num = $start; $num <= $end; $num++) {
-        switch($num) {
-            case ($num % 3 == 0 && $num % 5 == 0):
-                echo " Linianos ";
-                break;
-            case ($num % 3 == 0):
-                echo " Linio ";
-                break;
-            case ($num % 5 == 0):
-                echo " IT ";
-                break;
-            default:
-                echo " $num ";
-        }
-    }
+
+class NumberGenerator {
+	/**
+	* Prints "Linio" for multiplies of 3, "IT" for multiplies of 5 and "Linianos" for multiplies of both 3 & 5
+	*
+	* @param int $start
+	* @param int $end
+	*
+	* @return void
+	*/
+	public function numberPrint(int $start, int $end): void {
+		for ($num = $start; $num <= $end; $num++) {
+			switch($num) {
+				case ($num % 3 == 0 && $num % 5 == 0):
+					echo " Linianos ";
+					break;
+				case ($num % 3 == 0):
+					echo " Linio ";
+					break;
+				case ($num % 5 == 0):
+					echo " IT ";
+					break;
+				default:
+					echo " $num ";
+			}
+		}
+	}
 }
 
-numberPrint(1, 100); // Call the method
+$objNumGenerator = new NumberGenerator();
+$objNumGenerator->numberPrint(1, 100);
